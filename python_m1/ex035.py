8c36450e573d13191fdf286785334298e74d20ac
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-20 10:23:55 -0400

    Update ex035.py

--- python_m1/ex035.py
+++ python_m1/ex035.py
@@ -2,12 +2,12 @@
 a = float(input('Digite o comprimento da reta: '))
 b = float(input('Digite o comprimento da outra reta: '))
 c = float(input('Digite o comprimento da outra reta: '))
-
-if a > b + c:
+# Para ser triângulo tem que todos os lados serem  menores que a soma dos outros dois lados.
+if a >= b + c:
     print('Não é triângulo!')
-if b > a + c:
+if b >= a + c:
     print('Não é triângulo!')
-if c > a + b:
+if c >= a + b:
     print('Não é triângulo!')
 else:
     print('É triângulo!')
