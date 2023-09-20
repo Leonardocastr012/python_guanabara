49d2284172a323e3907b620bc19b73e715bd4f74
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-20 08:50:20 -0400

    Update ex028.py

---
+++ python_m1/ex028.py
@@ -0,0 +1,11 @@
+#Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
+#O programa deverá escrever na tela se o usuário venceu ou perdeu.
+from random import randint
+numeroSorteado = randint(1,5)
+print(numeroSorteado)
+numeroUsuario = int(input('Adivinhe um número entre 0 e 5: '))
+
+if (numeroUsuario == numeroSorteado):
+    print('Você acertou!')
+else:
+    print('Você errou!')
