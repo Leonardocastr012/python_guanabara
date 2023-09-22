dd329a6173ed6f58cec20f926ddc4a8ace053515
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-22 12:14:18 -0400

    Update ex043.py

--- python_m2/ex043.py
+++ python_m2/ex043.py
@@ -4,3 +4,19 @@
 – 25 até 30: Sobrepeso
 – 30 até 40: Obesidade
 – Acima de 40: Obesidade Mórbida'''
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura [ex.:1.86m]: '))
+imc = peso / pow(altura , 2)
+print(imc)
+if imc < 18.5:
+    print('Abaixo do Peso')
+elif 18.5 <= imc < 25:
+    print('Peso Ideal')
+elif 25 <= imc < 30:
+    print('Sobrepeso')
+elif 30 <= imc < 40:
+    print('Obesidade grau I')
+elif 30 <= imc < 40:
+    print('Obesidade grau II')
+else:
+    print('Obesidade Mórbida')
