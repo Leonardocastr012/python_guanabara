f27f6664ef6afce025a1c4afa53f214d007fe412
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-21 21:07:22 -0400

    Update ex036.py

--- python_m2/ex036.py
+++ python_m2/ex036.py
@@ -3,9 +3,9 @@
 valorCasa = float(input('Valor da casa? '))
 salario = float(input('Qual o valor do seu salário? '))
 ano = int(input('Em quantos anos pretende pagar? '))
-prestacao = valorCasa / (ano * 12)
+prestacao = valorCasa / (ano * 12) # ano * 12 porque as prestações são mensais
 print(f'Para pagar um casa de R${valorCasa:.2f} em {ano} ano(s) a prestação ficará R${prestacao:.2f}')
-if prestacao > (salario * 0.30):
+if prestacao > (salario * 0.30): # prestação maior que 30% do salário 
     print('Empréstimo NEGADO!')
 else:
     print('Empréstimo APROVADO!')
