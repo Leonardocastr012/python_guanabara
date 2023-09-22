70cb1fde3ac1e39440ea1cc431a781fc4b3d0cbe
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-22 12:37:20 -0400

    Update ex044.py

--- python_m2/ex044.py
+++ python_m2/ex044.py
@@ -3,3 +3,23 @@
 – à vista no cartão: 5% de desconto
 – em até 2x no cartão: preço formal 
 – 3x ou mais no cartão: 20% de juros'''
+valor = float(input('Digite o valor do produto: '))
+pagamento = int(input('''Escolha sua opção de pagamento:
+[1]À vista/cheque- 10% de desconto
+[2]À vista no cartão: 5% de desconto
+[3]Em até 2x no cartão: preço formal 
+[4]3x ou mais no cartão: 20% de juros
+Sua opção: '''))
+if pagamento == 1:
+    novoValor = valor - (valor * 0.1) # novo valor com 10% de desconto
+    print(f'Seu pagamento fica R${novoValor:.2f}!')
+elif pagamento == 2:
+    novoValor = valor - (valor * 0.05) # novo valor com 5% de desconto
+    print(f'Seu pagamento fica R${novoValor:.2f}!')
+elif pagamento == 3:
+    print(f'Seu pagamento fica 2x de R${valor / 2}!')
+if pagamento == 4:
+    parcelas = int(input('Qual a quantidade de parcelas[3 ou +]? '))
+    novoValorComJuros = valor + (valor * 0.20) # novo valor com 20% de juros
+    valorParcelado = novoValorComJuros / parcelas
+    print(f'Seu pagamento fica {parcelas}x de R${valorParcelado:.2f}!')
