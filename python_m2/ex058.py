903a4ac160e30012e707ece27a176cf8f892e0a7
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-26 10:45:30 -0400

    Update ex058.py

--- python_m2/ex058.py
+++ python_m2/ex058.py
@@ -1,2 +1,16 @@
 #Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10.
 #Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+from random import randint
+numeroSorteado = randint(1,10)
+print(numeroSorteado)
+count = 0
+while True:
+    numeroUsuario = int(input('Adivinhe um número entre 0 e 10: '))
+    if (numeroUsuario == numeroSorteado):
+        print('Você acertou!')
+        count += 1
+        break #Força a parada pois o loop é infinito
+    else:
+        print('Você errou!')
+        count += 1
+print(f'Foram necessários {count} palpites')
