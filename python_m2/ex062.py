53c221ebb2055e3e46f8c2ceb09723ea7a6a1e8e
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-26 17:43:52 -0400

    Update ex062.py

--- python_m2/ex062.py
+++ python_m2/ex062.py
@@ -1,14 +1,15 @@
 # Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. 
 #O programa encerrará quando ele disser que quer mostrar 0 termos.
 n = int(input('Quantos termos de fibonacci você quer mostrar? '))
-#fn = n1 = n2
+max = n
 n1 = 0
 n2 = 1
-
-while n != 0:
-    if n >= 3:
+fn = n1 + n2
+for c in range(1, n + 1):
+    if c == 1:
+        print(f'{n1}', end ='')
+    if c >= 2:
         fn = n1 + n2
         print(f'->{fn}', end='')
         n2 = n1
         n1= fn
-        n -= 1
