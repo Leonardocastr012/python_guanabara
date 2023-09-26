00f8c5bb86c75bb56b4ddc982335df36bcca66ed
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-26 10:50:22 -0400

    2nd version

--- python_m2/ex058.py
+++ python_m2/ex058.py
@@ -6,11 +6,16 @@ print(numeroSorteado)
 count = 0
 while True:
     numeroUsuario = int(input('Adivinhe um número entre 0 e 10: '))
-    if (numeroUsuario == numeroSorteado):
+    if (numeroUsuario == numeroSorteado): #Caso acerte
         print('Você acertou!')
         count += 1
         break #Força a parada pois o loop é infinito
-    else:
-        print('Você errou!')
-        count += 1
+
+    else: #Caso erre
+        if numeroSorteado > numeroUsuario: #Dizer caso número do usuário seja menor
+            print('Você errou! O número é MAIOR!')
+            count += 1
+        else: #Dizer caso número do usuário seja maior
+            print('Você errou! O número é MENOR!')
+            count += 1
 print(f'Foram necessários {count} palpites')
