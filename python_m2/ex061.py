6c20f3a0ca300ef90f4820038a4a2ce1db24bf8e
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-09-26 16:08:44 -0400

    Update ex061.py

--- python_m2/ex061.py
+++ python_m2/ex061.py
@@ -1,1 +1,9 @@
 # Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+a1 = float(input('Digite o primeiro termo: '))
+r = float(input('Qual a razão? '))
+n = 1 #posição do número na sequência
+while n != 11:  #para ir até 10
+    an = a1 + (n -1)*r
+    print(f'A termo {n}º da progressão é {an}')
+    n += 1
+print('Acabou')
