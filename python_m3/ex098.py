fa5900b10a67760fb70300e17ad09be763f820de
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-11-26 13:51:04 -0400

    I finished the exercise ex098.py

--- python_m3/ex098.py
+++ python_m3/ex098.py
@@ -1,32 +1,38 @@
 #Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:   
-from time import sleep
 def linha():
     print('-='*15)
 
-def contador():
-    # a) de 1 até 10, de 1 em 1
+def contador(inicio, fim, passo):
+    from time import sleep
     linha()
-    print('Contagem de 1 até 10, de 1 em 1')
-    for i in range(1, 11, 1):
-        print(f'{i}', end=' ')
-        sleep(1)
-    print('FIM!')
-    linha()
-    # b) de 10 até 0, de 2 em 2
-    print('Contagem de 10 até 0, de 2 em 2')
-    for i in range(10, -1, -2):#o passo tem que tá negativo para correr ao contrário
-        print(f'{i}', end=' ')
-        sleep(1)
-    print('FIM!')
-    linha()                                                                                                                                            
-    # c) uma contagem personalizada
-    print('Contagem Personalizada')
-    inicio = int(input('Início: '))
-    fim = int(input('Fim: '))
-    passo = int(input('Passo: '))
-    for i in range(inicio, fim, passo):
-        print(f'{i}', end=' ')
-        sleep(1)
-    print('FIM!')
+    #Teste lógico
+    if passo == 0:
+        passo = 1 
+    if passo < 0:
+        passo *= -1
+    
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+    sleep(2.5)
 
-contador()
+    if inicio < fim :
+        for i in range(inicio, fim + 1, passo):
+            print(f'{i}', end=' ', flush=True) # Uso o flush para não ativar o buffer de tela
+            sleep(0.5)
+        print('FIM!')
+    if inicio > fim :
+        for i in range(inicio, fim - 1, -passo): # tem que ser com o passo negativo para o for rodar do maior para o menor
+            print(f'{i}', end=' ', flush=True)
+            sleep(0.5)
+        print('FIM!')
+#A)de 1 até 10, de 1 em 1 
+contador(1, 10, 1)
+#B)de 10 até 0, de 2 em 2
+contador(10, 0, 2)
+
+linha()
+print('Contagem Personalizada')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
+linha()
