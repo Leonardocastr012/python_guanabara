c3c0fc36640bcf8e49e0ddfbd4ab37c3beafa6b1
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-11-28 18:39:47 -0400

    Update ex106.py

--- python_m3/ex106.py
+++ python_m3/ex106.py
@@ -1,2 +1,27 @@
 #Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. 
 #Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores.
+def cabeçalho():
+    nome = 'SISTEMA DE AJUDA PyHELP'
+    print('\033[0;30;42m~\033[m'*len(nome))
+    print(f'\033[0;30;42m{nome:^}\033[m')
+    print('\033[0;30;42m~\033[m'*len(nome))
+
+def cabeçalhoInterno(msg):
+    nome = 'ACESSANDO O MANUAL DE COMANDO: '
+    print('\033[0;30;46m~\033[m'*(len(nome)+len(msg)))
+    print(f'\033[0;30;46m{nome:^}{msg:<}\033[m')
+    print('\033[0;30;46m~\033[m'*(len(nome)+len(msg)))
+
+def menu(msg):
+    return f'\033[0;37;40m{help(msg)}\033[m'
+
+def PyHelp():
+    while True:
+        cabeçalho()
+        scan = str(input('Função ou biblioteca> '))
+        if scan.upper().strip() == 'FIM':
+            break
+        cabeçalhoInterno(scan)
+        menu(scan)
+
+PyHelp()
