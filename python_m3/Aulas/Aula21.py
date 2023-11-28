a21631777d86e1f5826c01515a743e4f6e04f067
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-11-27 21:08:25 -0400

    Update Aula21.py

--- python_m3/Aulas/Aula21.py
+++ python_m3/Aulas/Aula21.py
@@ -55,4 +55,40 @@ n1 = 2 #Escopo global, mas agora tá sem valor pelo comando global dentro da fun
 função()
 print(f'N1 fora vale {n1}')
 #Retorno de resultados
+linha()
+#ex5
+def somar2(a=0,b=0,c=0): #Caso c não tenha valor ele fica com valor 0, o parâmetro opcional pode ser colocado em qualquer parâmetro
+    s = a + b + c
+    return s
 
+r1 = somar2(3,2,5)
+r2 = somar2(1,7)
+r3 = somar2(4)
+print(f'Meus cálculos deram {r1}, {r2} e {r3}') #útil para quando for personalizar os resultados 
+print(somar2(5,10,15))
+
+#Prática
+#ex1
+def fatorial(num=1):
+    f = 1
+    for c in range(num,0,-1): #Aqui ele vai fazer um for decrescente 
+        f *= c
+    return f
+linha()
+n = int(input('Digite um número: '))
+print(f'{n}! = {fatorial(n)}')
+
+#ex2
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+linha()
+n = int(input('Digite um número: '))
+print(par(n))
+if par(n):
+    print('É par!')
+else:
+    print('Não é par!')
