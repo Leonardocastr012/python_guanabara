a6c7ca0058961b5035eb47f2edd547ec1c09e7bb
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-05 14:03:15 -0400

    Create cadastro.py

---
+++ python_m3/ex115/cadastro.py
@@ -0,0 +1,2 @@
+#Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo da texto simples.
+#O sistema só vai tar 2 opções: cadastrar uma nova passoa e listar todas as passoas cadastradas.
