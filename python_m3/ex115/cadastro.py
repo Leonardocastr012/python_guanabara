fd4775596c36163186910e724ebf6b3ffdd84b93
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-09 14:39:03 -0400

    Delete cadastro.py

--- python_m3/ex115/cadastro.py
+++
@@ -1,2 +0,0 @@
-#Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo da texto simples.
-#O sistema só vai tar 2 opções: cadastrar uma nova passoa e listar todas as passoas cadastradas.
